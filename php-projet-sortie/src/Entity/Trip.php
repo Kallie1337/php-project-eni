f3375a6e608702e4947726c230049eae88a5fd67
Marie-Morgane <mariemorgane.depre2018@campus-eni.fr>
2019-04-15 12:14:53 +0200

    add trip entity

---
+++ php-projet-sortie/src/Entity/Trip.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TripRepository")
+ */
+class Trip
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $beginDateTime;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $registrationEndDate;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $maxRegistration;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $infos;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getBeginDateTime(): ?\DateTimeInterface
+    {
+        return $this->beginDateTime;
+    }
+
+    public function setBeginDateTime(\DateTimeInterface $beginDateTime): self
+    {
+        $this->beginDateTime = $beginDateTime;
+
+        return $this;
+    }
+
+    public function getDuration(): ?string
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(string $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getRegistrationEndDate(): ?\DateTimeInterface
+    {
+        return $this->registrationEndDate;
+    }
+
+    public function setRegistrationEndDate(\DateTimeInterface $registrationEndDate): self
+    {
+        $this->registrationEndDate = $registrationEndDate;
+
+        return $this;
+    }
+
+    public function getMaxRegistration(): ?int
+    {
+        return $this->maxRegistration;
+    }
+
+    public function setMaxRegistration(int $maxRegistration): self
+    {
+        $this->maxRegistration = $maxRegistration;
+
+        return $this;
+    }
+
+    public function getInfos(): ?string
+    {
+        return $this->infos;
+    }
+
+    public function setInfos(string $infos): self
+    {
+        $this->infos = $infos;
+
+        return $this;
+    }
+}
