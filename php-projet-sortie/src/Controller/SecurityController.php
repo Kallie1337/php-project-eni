6fe56de40b0db8fdeadd26658c44684ed5ed77fd
LinoaHeartilly <alexandre.caniac93@gmail.com>
2019-04-15 14:33:37 +0200

    Login fini

--- php-projet-sortie/src/Controller/SecurityController.php
+++ php-projet-sortie/src/Controller/SecurityController.php
@@ -19,6 +19,6 @@ class SecurityController extends Controller
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('account/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 }
