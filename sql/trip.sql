2328c04830a455fdd4f5ab0c3766f1aea7ca667d
Kallie1337 <Kallie@nextborder.xyz>
2019-04-16 14:09:26 +0200

    Update trip.sql

--- sql/trip.sql
+++ sql/trip.sql
@@ -1,23 +1,11 @@
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('New York City', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à New York City');
+INSERT INTO `trip`(`name`, `begin_date_time`, `registration_end_date`,`current_registration` `max_registration`, `infos`) VALUES ('New York City', '2019/04/17', '2019/04/18','0', '15', 'Une sortie à New York City'); 
 
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('Los Angeles', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à Los Angeles');
+INSERT INTO `trip`(`name`, `begin_date_time`, `registration_end_date`,`current_registration` `max_registration`, `infos`) VALUES ('Los Angeles', '2019/04/17', '2019/04/18','0', '15', 'Une sortie à Los Angeles');  
 
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('Chicago', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à Chicago');
+INSERT INTO `trip`(`name`, `begin_date_time`, `registration_end_date`,`current_registration` `max_registration`, `infos`) VALUES ('Chicago', '2019/04/17', '2019/04/18','0', '15', 'Une sortie à Chicago');  
 
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('Houston', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à Houston');
+INSERT INTO `trip`(`name`, `begin_date_time`, `registration_end_date`,`current_registration` `max_registration`, `infos`) VALUES ('Houston', '2019/04/17', '2019/04/18','0', '15', 'Une sortie à Houston');  
 
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('Phoenix', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à Phoenix');
+INSERT INTO `trip`(`name`, `begin_date_time`, `registration_end_date`,`current_registration` `max_registration`, `infos`) VALUES ('Phoenix', '2019/04/17', '2019/04/18','0', '15', 'Une sortie à Phoenix');  
 
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('San Antonio', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à San Antonio');
-
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('San Diego', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à San Diego');
-
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('Dallas', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à Dallas');
-
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('San Jose', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à San Jose');
-
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('Austin', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à Austin');
-
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('Jacksonville', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à Jacksonville');
-
-INSERT INTO `trip`(`name`, `begin_date_time`, `duration`, `registration_end_date`, `max_registration`, `infos`) VALUES ('San Francisco', '2019/04/17', '7 jours', '2019/04/16', '15', 'Une sortie à San Francisco');
+INSERT INTO `trip`(`name`, `begin_date_time`, `registration_end_date`,`current_registration` `max_registration`, `infos`) VALUES ('San Francisco', '2019/04/17', '2019/04/18','0', '15', 'Une sortie à San Francisco');
