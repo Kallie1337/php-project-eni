aba7efc9e487575cef919695d4e11053ad252c92
Marie-Morgane <mariemorgane.depre2018@campus-eni.fr>
2019-04-16 16:17:53 +0200

    update css

--- php-projet-sortie/src/Form/TripType.php
+++ php-projet-sortie/src/Form/TripType.php
@@ -3,12 +3,18 @@
 namespace App\Form;
 
 use App\Entity\Trip;
+use Symfony\Component\DomCrawler\Field\TextareaFormField;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TripType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -17,8 +23,8 @@ class TripType extends AbstractType
             ->add('endDateTime')
             ->add('registrationEndDate')
             ->add('maxRegistration')
-            ->add('infos')
-        ;
+            ->add('currentRegistration')
+            ->add('infos', TextareaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
