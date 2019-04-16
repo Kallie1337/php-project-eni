e6a81df602a7cfb3170dcb8d31663cde5cd23e4b
Kallie1337 <Kallie@nextborder.xyz>
2019-04-16 09:56:27 +0200

    Create users.sql

---
+++ sql/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user`(`email`, `roles`, `password`, `username`) VALUES ('admin@sortie.com', '["admin"]', 'Password0*', 'admin');
+INSERT INTO `user`(`email`, `roles`, `password`, `username`) VALUES ('organisateur-1@sortie.com', '["organisateur"]', 'Password0*', 'organisateur-1');
+INSERT INTO `user`(`email`, `roles`, `password`, `username`) VALUES ('organisateur-2@sortie.com', '["organisateur"]', 'Password0*', 'organisateur-2');
+INSERT INTO `user`(`email`, `roles`, `password`, `username`) VALUES ('participant-1@sortie.com', '["participant"]', 'Password0*', 'participant-1');
+INSERT INTO `user`(`email`, `roles`, `password`, `username`) VALUES ('participant-2@sortie.com', '["participant"]', 'Password0*', 'participant-2');
